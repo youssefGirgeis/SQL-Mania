1182e954de0b0bbc9cc873ff47a6cd5157936809
Girgeis <Youssef.Girgeis@ontario.ca>
2021-04-28 21:50:44 -0400

    Solved: unique Replace Employee ID With The Unique Identifier

--- Leet_Code_SQL/unique_identifier.sql
+++ Leet_Code_SQL/unique_identifier.sql
@@ -22,4 +22,13 @@ Table: EmployeeUNI
 (id, unique_id) is the primary key for this table.
 Each row of this table contains the id and the corresponding unique id of an employee in the company.
 
+Write an SQL query to show the unique ID of each user, If a user doesn't have a unique ID replace just show null.
 **/
+
+---- Solution 1 -----
+SELECT 
+    e.name,
+    eu.unique_id
+FROM employees e 
+LEFT JOIN EmployeeUNI eu
+ON e.id = eu.id
