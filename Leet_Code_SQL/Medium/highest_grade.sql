ba8e3cee3b05d7bdfafb85153301fb6124f30d05
Girgeis <youssef.girgeis@ontario.ca>
2021-05-09 22:05:41 -0400

    Solved: Highest Grade For Each Student - two solution

---
+++ Leet_Code_SQL/Medium/highest_grade.sql
@@ -0,0 +1,57 @@
+/*
+Highest Grade For Each Student
+
+Table: Enrollments
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| student_id    | int     |
+| course_id     | int     |
+| grade         | int     |
++---------------+---------+
+(student_id, course_id) is the primary key of this table.
+
+Write a SQL query to find the highest grade with its corresponding course for each student. 
+In case of a tie, you should find the course with the smallest course_id. 
+The output must be sorted by increasing student_id.
+
+The query result format is in the following example:
+
+Enrollments table:
++------------+-------------------+
+| student_id | course_id | grade |
++------------+-----------+-------+
+| 2          | 2         | 95    |
+| 2          | 3         | 95    |
+| 1          | 1         | 90    |
+| 1          | 2         | 99    |
+| 3          | 1         | 80    |
+| 3          | 2         | 75    |
+| 3          | 3         | 82    |
++------------+-----------+-------+
+*/
+
+------------ Solution ------------
+
+SELECT 
+    student_id,
+    course_id,
+    grade
+FROM (
+    SELECT *,
+       RANK() OVER(PARTITION BY student_id ORDER BY grade DESC, course_id) ranks
+    FROM Enrollments
+) ranked_grades
+WHERE ranks = 1
+ORDER BY student_id
+/*
+Result table:
++------------+-------------------+
+| student_id | course_id | grade |
++------------+-----------+-------+
+| 1          | 2         | 99    |
+| 2          | 2         | 95    |
+| 3          | 3         | 82    |
++------------+-----------+-------+
+*/
