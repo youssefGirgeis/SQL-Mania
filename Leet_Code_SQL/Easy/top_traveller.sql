39614e006292a566c3aa4979cd77d65e124f5b1d
Girgeis <Youssef.Girgeis@ontario.ca>
2021-05-11 00:42:11 -0400

    Solved: Top Travellers

---
+++ Leet_Code_SQL/Easy/top_traveller.sql
@@ -0,0 +1,88 @@
+/*
+Table: Users
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key for this table.
+name is the name of the user.
+ 
+
+Table: Rides
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| user_id       | int     |
+| distance      | int     |
++---------------+---------+
+id is the primary key for this table.
+user_id is the id of the user who travelled the distance "distance".
+ 
+
+Write an SQL query to report the distance travelled by each user.
+
+Return the result table ordered by travelled_distance in descending order,
+if two or more users travelled the same distance, order them by their name in ascending order.
+
+The query result format is in the following example.
+
+ 
+
+Users table:
++------+-----------+
+| id   | name      |
++------+-----------+
+| 1    | Alice     |
+| 2    | Bob       |
+| 3    | Alex      |
+| 4    | Donald    |
+| 7    | Lee       |
+| 13   | Jonathan  |
+| 19   | Elvis     |
++------+-----------+
+
+Rides table:
++------+----------+----------+
+| id   | user_id  | distance |
++------+----------+----------+
+| 1    | 1        | 120      |
+| 2    | 2        | 317      |
+| 3    | 3        | 222      |
+| 4    | 7        | 100      |
+| 5    | 13       | 312      |
+| 6    | 19       | 50       |
+| 7    | 7        | 120      |
+| 8    | 19       | 400      |
+| 9    | 7        | 230      |
++------+----------+----------+
+*/
+ --------- Solution ------------
+SELECT
+    u.name,
+    COALESCE(SUM(r.distance), 0) travelled_distance
+FROM users u 
+LEFT JOIN rides r 
+ON u.id = r.user_id
+GROUP BY name
+ORDER BY travelled_distance DESC, name
+
+
+/*
+Result table:
++----------+--------------------+
+| name     | travelled_distance |
++----------+--------------------+
+| Elvis    | 450                |
+| Lee      | 450                |
+| Bob      | 317                |
+| Jonathan | 312                |
+| Alex     | 222                |
+| Alice    | 120                |
+| Donald   | 0                  |
++----------+--------------------+
+*/
