e9a5d06e2a2f867cc456552c0dee7055f01c150c
Girgeis <youssef.girgeis@ontario.ca>
2021-05-28 01:52:39 -0400

    Solved: Find the Quiet Students in All Exams

---
+++ Leet_Code_SQL/Hard/quiet_students_in_all_exams.sql
@@ -0,0 +1,97 @@
+/*
+Find the Quiet Students in All Exams
+
+Table: Student
+
++---------------------+---------+
+| Column Name         | Type    |
++---------------------+---------+
+| student_id          | int     |
+| student_name        | varchar |
++---------------------+---------+
+student_id is the primary key for this table.
+student_name is the name of the student.
+ 
+
+Table: Exam
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| exam_id       | int     |
+| student_id    | int     |
+| score         | int     |
++---------------+---------+
+(exam_id, student_id) is the primary key for this table.
+Student with student_id got score points in exam with id exam_id.
+ 
+
+A "quite" student is the one who took at least one exam and didn't score neither the high score nor the low score.
+
+Write an SQL query to report the students (student_id, student_name) being "quiet" in ALL exams.
+
+Don't return the student who has never taken any exam. Return the result table ordered by student_id.
+
+The query result format is in the following example.
+
+ 
+
+Student table:
++-------------+---------------+
+| student_id  | student_name  |
++-------------+---------------+
+| 1           | Daniel        |
+| 2           | Jade          |
+| 3           | Stella        |
+| 4           | Jonathan      |
+| 5           | Will          |
++-------------+---------------+
+
+Exam table:
++------------+--------------+-----------+
+| exam_id    | student_id   | score     |
++------------+--------------+-----------+
+| 10         |     1        |    70     |
+| 10         |     2        |    80     |
+| 10         |     3        |    90     |
+| 20         |     1        |    80     |
+| 30         |     1        |    70     |
+| 30         |     3        |    80     |
+| 30         |     4        |    90     |
+| 40         |     1        |    60     |
+| 40         |     2        |    70     |
+| 40         |     4        |    80     |
++------------+--------------+-----------+
+*/
+
+--------- Solution ---------------
+
+SELECT
+    DISTINCT s.student_id,
+    s.student_name
+FROM student s 
+JOIN exam e 
+ON s.student_id = e.student_id
+WHERE s.student_id NOT IN (
+    SELECT
+        DISTINCT student_id
+    FROM (
+        SELECT
+            student_id,
+            score,
+            MIN(score) OVER(PARTITION BY exam_id) min_score,
+            MAX(score) OVER(PARTITION BY exam_id) max_score
+        FROM exam
+        ) t1
+    WHERE score = min_score or score = max_score
+)
+ORDER BY 1
+
+/*
+Result table:
++-------------+---------------+
+| student_id  | student_name  |
++-------------+---------------+
+| 2           | Jade          |
++-------------+---------------+
+*/
